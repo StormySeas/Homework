ce70e65123635f70da416ca27f867d0f4d169e2f
Julian <julianboyd0@gmail.com>
2018-09-14 15:49:51 -0500

    commit 1

---
+++ 9-13-homework/java/app.js
@@ -0,0 +1,21 @@
+console.log('hi there');
+
+
+
+const word = 'Radar'
+const checkPalindrome =  (word) => {
+word = word.split('')
+console.log(word);
+word.reverse()
+console.log(word);
+word.join()
+console.log(word);
+
+}
+
+
+
+
+
+
+console.log(checkPalindrome('hello'));
