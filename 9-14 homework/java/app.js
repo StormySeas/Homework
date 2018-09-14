97b9cbc2373c0b2482fe33535df7cffc9ba97376
Julian <julianboyd0@gmail.com>
2018-09-14 16:04:15 -0500

    commit 2

---
+++ 9-14 homework/java/app.js
@@ -0,0 +1,32 @@
+// // How do we assign a value to a variable?
+// var = 40
+// // How do we change the value of a variable?
+// let (var) = 55
+// // How do we assign an existing variable to a new variable?
+//
+// // Remind me, what are declare, assign, and define?
+// declaring is giving the value of a variable
+// assigning is  changing properties from a source object to a target object
+// objects are defined by the functions attached to them
+// // What is pseudocoding and why should you do it?
+// psuedocoding is writing out your plan for the code in "plain english" to better understand your next steps
+// // What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+// 80% thinking, 20% coding
+//
+
+//
+// Create a variable called firstVariable.
+// Assign it the value of the string "Hello World"
+// Change the value of this variable to some number.
+// Store the value of firstVariable in a new variable called secondVariable
+// Change the value of secondVariable to any string.
+// -1000
+// What is the value of firstVariable?
+// Create a variable called yourName and set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourName so that it returns a new string with them concatenated.
+// var firstVariable = 'Hello World'
+// var firstVariable = "Goodbye World"
+// var firstVariable = 1000
+// var secondVariable = firstVariable
+// console.log(secondVariable);
+// var yourName = 'Julian'
+// console.log('Hello, my name is ' + yourName);
